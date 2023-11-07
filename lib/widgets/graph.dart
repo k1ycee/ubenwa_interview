a34ad15f15f841adf02d3378ac7abba102c234b4
k1ycee <chiagozieani145@gmail.com>
2023-11-07 07:59:19 +0100

    A more optimized codebase

--- lib/widgets/graph.dart
+++ lib/widgets/graph.dart
@@ -64,22 +64,58 @@ class HourlyBreakDownChartState extends State<HourlyBreakDownChart> {
   }
 
   final data = [
-    2.0,
-    5.0,
-    4.0,
-    2.5,
-    3.0,
-    3.0,
-    2.0,
-    5.0,
-    4.0,
-    2.5,
-    3.0,
-    3.0,
+    (
+      0,
+      2.0,
+    ),
+    (
+      1,
+      2.0,
+    ),
+    (
+      2,
+      2.0,
+    ),
+    (
+      3,
+      2.0,
+    ),
+    (
+      4,
+      2.0,
+    ),
+    (
+      5,
+      2.0,
+    ),
+    (
+      6,
+      2.0,
+    ),
+    (
+      7,
+      2.0,
+    ),
+    (
+      8,
+      2.0,
+    ),
+    (
+      9,
+      2.0,
+    ),
+    (
+      10,
+      2.0,
+    ),
+    (
+      11,
+      2.0,
+    ),
   ];
 
-  List<BarChartGroupData> showingGroups(List<dynamic> data) {
-    return data.map((e) => makeGroupData(e.toInt(), e)).toList();
+  List<BarChartGroupData> showingGroups() {
+    return data.map((e) => makeGroupData(e.$1, e.$2)).toList();
   }
 
   BarChartData mainBarData() {
@@ -108,30 +144,28 @@ class HourlyBreakDownChartState extends State<HourlyBreakDownChart> {
       borderData: FlBorderData(
         show: false,
       ),
-      barGroups: showingGroups(data),
+      barGroups: showingGroups(),
       gridData: const FlGridData(show: false),
     );
   }
 
-  Widget getTitles(double value, TitleMeta meta) {
+  Widget getTitles(
+    double value,
+    TitleMeta meta,
+  ) {
     const style = TextStyle(
       color: white,
-      // fontWeight: FontWeight.bold,
       fontSize: 8,
     );
     Widget text;
-
-    List<String> timeRanges = List.generate(12, (index) {
-      int startHour = index * 2;
-      int endHour = startHour + 2;
-      String timeRange = '$startHour:00/\n$endHour:00';
-      return timeRange;
-    });
-
     int intValue = value.toInt();
 
-    if (intValue >= 0 && intValue < timeRanges.length) {
-      text = Text(timeRanges[intValue], style: style);
+    int startHour = intValue * 2;
+    int endHour = startHour + 2;
+    String timeRange = '$startHour:00/\n$endHour:00';
+
+    if (intValue >= 0) {
+      text = Text(timeRange, style: style);
     } else {
       text = const Text('', style: style);
     }
@@ -142,6 +176,4 @@ class HourlyBreakDownChartState extends State<HourlyBreakDownChart> {
       child: text,
     );
   }
-
-  List<Widget> text = [];
 }
